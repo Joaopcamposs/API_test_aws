14218dbddb7e0ad2f7a7d2fe33d7e20e04f5d5d2
Joaopcamposs <joaopcamposs@outlook.com>
2021-11-03 10:29:53 -0300

    Atualizacao da api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
 
 const app = express();
 
 app.get('/',(request, response)=>{
-    return response.json({message: 'Server is up!'})
+    return response.json({message: 'Server is up!'});
+})
+
+app.get('/atualizou', (request, response)=>{
+    return response.json({message: 'API atualizada!'});
+})
+
+app.post('/teste', (request, response)=>{
+    const {name, date} = request.body;
+
+    return response.json({name, date});
 })
 
 app.listen(3333)
